0129fba7a6265dde7158aebb29941c1be848fcd8
jonlamb-gh <jonlambusn@gmail.com>
2018-05-06 11:15:46 -0700

    WIP xsmessage-dump

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xsens-tools
 A place for my random Xsens tools/apps/libs.
+
+## TODO's
+
+- use `popt` or something to add CLI to `tools/xsmessage-dump`
--- tools/xsmessage-dump/src/main.c
+++ tools/xsmessage-dump/src/main.c
@@ -7,9 +7,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
 #include <assert.h>
 #include "xsparser/xsparser.h"
 #include "xsparser/xsmessage.h"
+
+static volatile sig_atomic_t g_exit_signaled;
+
+static void sig_handler(
+    const int sig)
+{
+    if(sig == SIGINT)
+    {
+        g_exit_signaled = 1;
+    }
+}
+
+static void register_sigint(void)
+{
+    struct sigaction act;
+
+    memset(&act, 0, sizeof(act));
+    act.sa_flags = SA_RESTART;
+    act.sa_handler = sig_handler;
+
+    if(sigaction(SIGINT, &act, 0) < 0)
+    {
+        perror("sigaction(SIGINT)");
+        exit(1);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -20,12 +48,19 @@ int main(int argc, char **argv)
             XS_MAXGARBAGE);
     assert(rx_buffer != NULL);
 
+    g_exit_signaled = 0;
+    register_sigint();
+
     xsparser_init(
             rx_buffer,
             XS_MAXGARBAGE,
             &parser);
 
-    assert(parser.state == XSPARSER_STATE_PREAMBLE);
+    while(g_exit_signaled == 0)
+    {
+        // TODO
+        (void) sleep(1);
+    }
 
     free(rx_buffer);
 
